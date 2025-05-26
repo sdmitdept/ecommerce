5a5c39d10e39b4e613cd053f9915a0b200e28d6d
rdesviantina <rdesviantina@gmail.com>
2025-05-26 02:52:46 +0000

    System generated commit, initial commit

---
+++ pages/Import Data/jsobjects/utils/utils.js
@@ -0,0 +1,30 @@
+export default {
+	
+	dashboardMetrics: async () => {
+		const orders = await getorder.run();
+
+			const allOrders = orders.length
+		// const fulfilledOrders = orders.filter(o => o.status === 'DELIVERED').length;
+		// const unfulfilledOrders = orders.filter(o => o.status === 'UNFULFILLED').length;
+		// const cancelledOrders = orders.filter(o => o.status === 'CANCELLED').length;
+		// const shippedOrders = orders.filter(o => o.status === 'SHIPPED').length;
+		// const packedOrders = orders.filter(o => o.status === 'PACKED').length;
+		// const totalOrderValue = orders.reduce((a, b) => a + b.total, 0);
+		// const formattedOrderValueAmount = totalOrderValue.toLocaleString('en-US', { style: 'currency', currency: 'USD' }).slice(0, -3);
+
+		return {
+			allOrders
+			//fulfilledOrders,
+			//unfulfilledOrders,
+			//cancelledOrders,
+			//shippedOrders,
+			//packedOrders,
+			//returnsCount: returnsCount[0].count,
+			//totalOrderValue: formattedOrderValueAmount,
+		}
+		
+//revenueChart: async () => {
+		//const revenueByMonth = await getRevenueByMonth.run();
+		
+	}	
+	}
